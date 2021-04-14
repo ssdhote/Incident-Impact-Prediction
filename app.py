be769c8fc852a99ee5f6da2b47be87edf977388a
Shweta dhote <82504583+ssdhote@users.noreply.github.com>
2021-04-14 15:52:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output= ""
+    
+    if prediction[0]==1:
+        output="High"
+    elif prediction[0]==3:
+        output="Low"
+    else:
+        output="Medium"
+ 
+
+    return render_template('index.html', prediction_text='Incident Impact is {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
